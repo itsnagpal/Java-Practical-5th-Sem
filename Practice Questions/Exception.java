ad27b7361cfd59b2172e2be0ce4dca2b18341399
Ishpreet Singh <itsnagpals@gmail.com>
2024-11-06 14:13:52 +0530

    Exception handling initial files

--- Practice Questions/Exception.java
+++ Practice Questions/Exception.java
@@ -2,16 +2,18 @@
 public class Exception {
     public static void main(String[] args) {
         try {
-            int a[] = new int[5];
-            a[6] = 30 / 1;
-            a[5] = 30 / 0;  // This line will throw an ArithmeticException before ArrayIndexOutOfBoundsException
+            int a[] = new int[10];
+            a[11] = 76 / 1;
+            a[10] = 76 / 0;  
             
         } 
         catch (ArithmeticException e) {
-            System.out.println("Arithmetic Exception occurs");
+            System.out.println("Arithmetic Exception: " + e);
+            System.out.println("");
         } 
         catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("ArrayIndexOutOfBounds Exception occurs");
+            System.out.println("ArrayIndexOutOfBounds Exception: " + "" +e);
+            System.out.println("");
         }
         System.out.println("rest of the code");
     }
